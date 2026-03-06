9f52390c38086199345eae575bb0dabd4825e8b1
gustavrl <gustavrl657@gmail.com>
2026-03-06 13:58:53 -0600

    json

---
+++ json/json.js
@@ -0,0 +1,11 @@
+// buscar la respuesta 
+
+fetch('json.json')
+.then(Response => Response.json())
+.then (json => {
+    document.getElementById('adn').innerHTML=json.frase1;
+    document.getElementById('id').innerHTML=json.frase2;
+    document.getElementById('fer').innerHTML=json.frase3;
+    document.getElementById('numero').innerHTML=json.cantidad1;
+    console.log(json.frase1)
+})
